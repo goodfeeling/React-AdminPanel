5ce6bf9a05d318b8706a4c3e49b8865a2de1ac48
yaozhiwen <929089994@qq.com>
2025-07-09 23:09:30 +0800

    暂存

---
+++ src/api/services/dictionaryDetailService.ts
@@ -0,0 +1,40 @@
+import apiClient from "../apiClient";
+
+import type { Api, ApiGroup, PageList } from "#/entity";
+
+export enum ApiClient {
+  Api = "/api",
+  SearchApi = "/api/search",
+  GroupsApi = "/api/groups",
+  DeleteBatch = "/api/batch",
+}
+const getApis = () => apiClient.get<Api[]>({ url: `${ApiClient.Api}` });
+const updateApi = (id: number, apiInfo: Api) =>
+  apiClient.put<Api>({ url: `${ApiClient.Api}/${id}`, data: apiInfo });
+
+const createApi = (apiInfo: Api) =>
+  apiClient.post<Api>({ url: `${ApiClient.Api}`, data: apiInfo });
+
+const searchPageList = (searchStr: string) =>
+  apiClient.get<PageList<Api>>({
+    url: `${ApiClient.SearchApi}?${searchStr}`,
+  });
+
+const deleteApi = (id: number) =>
+  apiClient.delete<string>({ url: `${ApiClient.Api}/${id}` });
+
+const deleteBatch = (ids: number[]) =>
+  apiClient.post<number>({ url: `${ApiClient.DeleteBatch}`, data: { ids } });
+
+const getApiGroups = () =>
+  apiClient.get<ApiGroup>({ url: `${ApiClient.GroupsApi}` });
+
+export default {
+  updateApi,
+  searchPageList,
+  createApi,
+  deleteApi,
+  getApis,
+  getApiGroups,
+  deleteBatch,
+};
